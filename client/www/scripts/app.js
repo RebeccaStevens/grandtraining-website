a260c34d9e893145f5af9848b48b0de04c6a9e5c
slaymaster3000 <slaymaster3000@gmail.com>
2015-10-31 19:11:51 +1300

    created small screen display

--- client/www/scripts/app.js
+++ client/www/scripts/app.js
@@ -3,7 +3,45 @@
 
   // Grab a reference to our auto-binding template and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
+  let app = document.querySelector('#app');
+
+  app.menuItems = [
+    {
+      title: 'Home',
+      href: '/',
+      imgsrc: 'images/desktop-icons/home.png'
+    },
+    {
+      title: 'Courses',
+      href: '/courses',
+      imgsrc: 'images/desktop-icons/courses.png'
+    },
+    {
+      title: 'Bookings',
+      href: '/bookings',
+      imgsrc: 'images/desktop-icons/bookings.png'
+    },
+    {
+      title: 'F.A.Q.',
+      href: '/faq',
+      imgsrc: 'images/desktop-icons/faq.png'
+    },
+    {
+      title: 'Contact',
+      href: '/contact',
+      imgsrc: 'images/desktop-icons/contact.png'
+    },
+    {
+      title: 'About',
+      href: '/about',
+      imgsrc: 'images/desktop-icons/about.png'
+    },
+    {
+      title: 'Broken',
+      href: '/broken',
+      imgsrc: 'images/desktop-icons/broken.png'
+    }
+  ];
 
   /**
    * Show a toast message to the user.
@@ -36,13 +74,17 @@
     }
   };
 
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
+  /**
+   * Listen for template bound event to know when bindings
+   * have resolved and content has been stamped to the page
+   */
   app.addEventListener('dom-change', function() {
     console.log('Our app is ready to rock!');
   });
 
-  // Listen for when imports are loaded and elements have been registered
+  /**
+   * Listen for when imports are loaded and elements have been registered
+   */
   window.addEventListener('WebComponentsReady', function() {
 
   });
