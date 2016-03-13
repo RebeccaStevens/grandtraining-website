b745274ceaba790cde71d602497265081c25bef3
slaymaster3000 <slaymaster3000@gmail.com>
2016-03-14 08:57:17 +1300

    added Registered field to Families

--- mysite/code/Family.php
+++ mysite/code/Family.php
@@ -2,6 +2,10 @@
 
 class Family extends Person {
 
+    private static $db = array(
+        'Registered' => 'Boolean'
+    );
+
     private static $has_many = array(
         'CareGivers' => 'CareGiver',
         'Children' => 'Student',
