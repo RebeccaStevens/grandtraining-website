b1f7d08ad37449d676376e745a19ac18a1c78f7f
slaymaster3000 <slaymaster3000@gmail.com>
2016-03-15 08:05:13 +1300

    initial setup for adding attendees to a scheduled course

--- mysite/code/BookingsPage.php
+++ mysite/code/BookingsPage.php
@@ -6,4 +6,63 @@ class BookingsPage extends Page {
 
 class BookingsPage_Controller extends Page_Controller {
 
+	private static $allowed_actions = array(
+		'add',
+		'AddStudentForm'
+	);
+
+	public function add(SS_HTTPRequest $request) {
+		$scheduledCourseID = $request->getVar('scid');
+		if ($scheduledCourseID === null) {
+			$this->httpError(404);
+		}
+
+		$scheduledCourse = ScheduledCourse::get()->filter(array('ID' => $scheduledCourseID))[0];
+		if ($scheduledCourse === null) {
+			$this->httpError(404);
+		}
+
+		return array(
+            'ScheduledCourse' => $scheduledCourse
+        );
+	}
+
+	public function AddStudentForm() {
+
+        // Create fields
+        $fields = new FieldList(
+            new TextField('FirstName', 'First Name'),
+			new TextField('Surname', 'Surname'),
+			new NumericField('Age', 'Age')
+        );
+
+        // Create actions
+        $actions = new FieldList(
+            new FormAction('addBooking', 'Submit')
+        );
+
+		$required = new RequiredFields(
+			'StudentFirstName',
+			'StudentSurname',
+			'StudentAge'
+		);
+
+        return new Form($this, __FUNCTION__, $fields, $actions, $required);
+    }
+
+	public function addStudent($data, $form) {
+		$student = Student::create();
+		$form->saveInto($student);
+	    // $student->Family =
+
+		Session::add_to_array('students', $student);
+
+		// Debug::show($student);
+		// die();
+
+	    // $form->sessionMessage('Message', 'good');
+
+	    return $this->redirectBack();
+	}
+
 }
---
+++ themes/grandtraining/templates/Layout/BookingsPage_add.ss
@@ -0,0 +1,13 @@
+<section data-route="$Route" data-title="$Title - $SiteConfig.Title">
+  <h1>$ScheduledCourse.Course.Title</h1>
+  <div>$ScheduledCourse.StartDay.Long - $ScheduledCourse.EndDay.Long</div>
+  <div>$ScheduledCourse.Price.Nice</div>
+  <br>
+  <div>
+    Attendees:<br>
+    ...<br>...<br>...<br>
+  </div>
+  <br>
+  Add Attendee:
+  $AddStudentForm
+</section>
